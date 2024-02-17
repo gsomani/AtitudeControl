4e1e29a0ee05c1a543439560c94ca4e4b6d36f74
Gaurav Somani <gaurav@astrome.co>
2024-02-17 16:55:49 +0530

    generate full cube

--- python/DynamicsTest.py
+++ python/DynamicsTest.py
@@ -10,10 +10,11 @@ class TestSpacecraftDynamics(unittest.TestCase):
     def setUp(self):
         self.I = np.diag([900, 800, 600])
         self.qInit = np.array([1.0, 0.0, 0.0, 0.0])  # Initial orientation
-        self.omegaInit = np.array([0.1, -0.05, 0.0])
+        self.omegaInit = np.array([0.0, 0.0, 0.0])
         self.dt = 0.01
-        self.simTime = 5.0
+        self.simTime = 100
 
+    '''
     def testZeroTorque(self):
         T = np.array([0.0, 0.0, 0.0])
 
@@ -24,7 +25,9 @@ class TestSpacecraftDynamics(unittest.TestCase):
         # maxDeviation = 0.01  # Adjust tolerance as needed
         # self.assertTrue(np.all(np.abs(omegaInit - omegaHistory[-1]) < maxDeviation))
 
-    def testConstantTorque(self): # In one axis
+    '''
+
+    def tesConstantTorque(self): # In one axis
         T = np.array([2.0, 0.0, 0.0])
 
         omegaHistory, quatHistory = self.simulate(T)
@@ -34,8 +37,16 @@ class TestSpacecraftDynamics(unittest.TestCase):
         # maxDeviation = 0.01  # Adjust tolerance as needed
         # self.assertTrue(np.all(np.abs(omegaInit - omegaHistory[-1]) < maxDeviation))
 
+
+    def testTwoAxisConstantTorque(self):
+        T = np.array([2.0, 2.0, 0.0])
+        omegaHistory, quatHistory = self.simulate(T)
+        self.plot(omegaHistory, quatHistory, 'Constant-Torque2Axis')
+
+
     def plot(self, omegaHistory, quatHistory, title=''):
         time = np.arange(0, self.simTime, self.dt)
+
         plt.figure()
         plt.plot(time, omegaHistory[:, 0], label='Roll')  # Plot roll rate
         plt.plot(time, omegaHistory[:, 1], label='Pitch')  # Plot pitch rate
@@ -50,28 +61,42 @@ class TestSpacecraftDynamics(unittest.TestCase):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
 
-        # Define vertices of a simple shape (e.g., a cube)
-        vertices = np.array([[-1, -1, -1], [1, -1, -1], [1, 1, -1], [-1, 1, -1],
-                            [-1, -1, 1], [1, -1, 1], [1, 1, 1], [-1, 1, 1]])
+        vertices = np.array([[i,j,k] for i in [-1,1] for j in [-1,1] for k in [-1,1]])
+        edges = []
+
+        for i,v in enumerate(vertices):
+          for j,u in enumerate(vertices[i+1:]):
+            if np.linalg.norm(u-v) == 2:
+              edges.append([i,i+1+j])
 
         # Initial plot setup
         rotatedVertices = vertices.T  # Assume no initial rotation for simplicity
-        lines = ax.plot(rotatedVertices[0,:], rotatedVertices[1,:], rotatedVertices[2,:])
+        #lines = [ax.plot(rotatedVertices[0,i*4:(i+1)*4], rotatedVertices[1,i*4:(i+1)*4], rotatedVertices[2,i*4:(i+1)*4]) for i in range(3)]
+        lines = []
+        for e in edges:
+          l = np.array([ rotatedVertices[:,e[i]] for i in range(2) ]).T
+          lines.append(ax.plot(*l))
+
+       #lines = [ax.plot( rotatedVertices[0,i], rotatedVertices[1,i], rotatedVertices[2,i*4:(i+1)*4]) for i in range(3)]
+
+
+        #lines = [ax.plot(rotatedVertices[0,:], rotatedVertices[1,:], rotatedVertices[2,:])]
         ax.set_xlim([-2, 2])
         ax.set_ylim([-2, 2])
         ax.set_zlim([-2, 2])
 
         # Update vertices at a few simulation instants
-        steps = 5
+        steps = 100
         print(quatHistory)
-        for i in range(0, len(time), len(time) // steps):
+        for i in range(0, len(time),50):
             rotationMatrix = quaternionToRotationMatrix(quatHistory[i])
-            # ... (Calculate rotation matrix based on attitude at timestep i)
             rotatedVertices = rotationMatrix @ vertices.T
 
-            # Update 3D plot
-            lines[0].set_data(rotatedVertices[0,:], rotatedVertices[1,:])
-            lines[0].set_3d_properties(rotatedVertices[2,:])
+            for e,l in zip(edges,lines):
+                rl = np.array([ rotatedVertices[:,e[i]] for i in range(2) ]).T
+                l[0].set_data(*rl[:2])
+                l[0].set_3d_properties(rl[2])
+
             plt.pause(0.1)
 
         plt.show()
