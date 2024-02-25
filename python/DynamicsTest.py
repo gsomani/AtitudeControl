fa377add93f8d0d1c0447b562a356c6d4ec9df7d
Gaurav Somani <gaurav@astrome.co>
2024-02-25 22:31:04 +0530

    clean up indentation

--- python/DynamicsTest.py
+++ python/DynamicsTest.py
@@ -6,42 +6,41 @@ from Quaternion import calculateCurrentOrientation
 
 class TestSpacecraftDynamics(unittest.TestCase):
 
-    def setUp(self):
-        self.I = np.diag([900, 800, 600])
-        self.qInit = np.array([1.0, 0.0, 0.0, 0.0])  # Initial orientation
-        self.omegaInit = np.array([0.0, 0.0, 0.0])
-        self.dt = 0.01
-        self.simTime = 100
+  def setUp(self):
+    self.I = np.diag([900, 800, 600])
+    self.qInit = np.array([1.0, 0.0, 0.0, 0.0])  # Initial orientation
+    self.omegaInit = np.array([0.0, 0.0, 0.0])
+    self.dt = 0.01
+    self.simTime = 100
 
-    def tesConstantTorque(self): # In one axis
-        T = np.array([2.0, 0.0, 0.0])
+  def tesConstantTorque(self): # In one axis
+    T = np.array([2.0, 0.0, 0.0])
 
-        omegaHistory, quatHistory = self.simulate(T)
-        Plotter.plot3D(quatHistory)
+    quatHistory = self.simulate(T)
+    Plotter.plot3D(quatHistory)
 
-    def testTwoAxisConstantTorque(self):
-        T = np.array([2.0, 2.0, 0.0])
-        omegaHistory, quatHistory = self.simulate(T)
-        Plotter.plot3D(quatHistory)
+  def testTwoAxisConstantTorque(self):
+    T = np.array([2.0, 2.0, 0.0])
+    quatHistory = self.simulate(T)
+    Plotter.plot3D(quatHistory)
 
-    def simulate(self, T):
-        """
-        Simulates spacecraft dynamics.
-        """
-        omegaHistory = [self.omegaInit]
-        quatHistory = [self.qInit]
+  def simulate(self, T):
+    """
+    Simulates spacecraft dynamics.
+    """
+    omega = self.omegaInit
+    quatHistory = [self.qInit]
 
-        for t in np.arange(self.dt, self.simTime, self.dt):
-            omegadot = angularAcceleration(T, omegaHistory[-1], self.I)
+    for t in np.arange(self.dt, self.simTime, self.dt):
+      omegadot = angularAcceleration(T, omega, self.I)
 
-            omega = omegaHistory[-1] + omegadot * self.dt
+      omega += omegadot * self.dt
 
-            q = calculateCurrentOrientation(quatHistory[-1], omega, self.dt)
+      q = calculateCurrentOrientation(quatHistory[-1], omega, self.dt)
 
-            omegaHistory.append(omega)
-            quatHistory.append(q)
+      quatHistory.append(q)
 
-        return np.array(omegaHistory), np.array(quatHistory)
+    return np.array(quatHistory)
 
 
 if __name__ == '__main__':
